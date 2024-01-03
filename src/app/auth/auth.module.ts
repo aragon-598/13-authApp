ad5f224e4df135f0435849588a480fdbcc810b58
Alejandro Aragon <aragondru155@gmail.com>
2024-01-02 20:50:53 -0600

    login & register pages  || diseño

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 
 @NgModule({
   declarations: [
   
+    AuthLayoutComponent,
     LoginPageComponent,
-       RegisterPageComponent,
-       AuthLayoutComponent,
+    RegisterPageComponent,
   ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+
+  public fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    name:['',[Validators.required]],
+    email:['',[Validators.required,Validators.email]],
+    password: ['',[Validators.required,Validators.minLength(6)]]
+  });
+
+  onSubmit(){
+    console.log(this.myForm.value);
+    
+  }
 
 }
