433574fd8efd09d7b5c4cb86eb75edcb052157a9
Alejandro Aragon <aragondru155@gmail.com>
2024-01-03 14:55:17 -0600

    logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -46,7 +46,10 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if(!token) return of(false);
+    if(!token) {
+      this.logout(  )
+      return of(false);
+    }
 
     const headers = new HttpHeaders()
                         .set('Authorization',`Bearer ${token}`);
@@ -61,5 +64,12 @@ export class AuthService {
                         return of(false);
                       })
                     );
+  }
+
+
+  logout(){
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
+    localStorage.removeItem('token');
   }
 }
